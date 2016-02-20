50262d3fa379cda62eb4ac686f5d8ea1aab5a639
eele94 <eele_mailadres@hotmail.com>
2016-02-20 18:29:00 +0100

    Base
    base project

---
+++ app/src/boot.ts
@@ -0,0 +1,6 @@
+/// <reference path="_all.ts" />
+
+module PortfolioManagerApp{
+    angular.module('portfolioManagerApp',['ngMaterial'])
+    .controller('mainController', MainController);
+}
---
+++ app/src/controllers/mainController.ts
@@ -0,0 +1,12 @@
+/// <reference path="../_all.ts" />
+
+module PortfolioManagerApp{
+    export class MainController{
+        
+        constructor(){
+            
+        }
+        
+        message: string = "Hello from controller";
+    }
+}
